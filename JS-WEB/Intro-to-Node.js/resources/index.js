4ad3daf3fd0dc2b5874f3843c51afb683c76a7b5
Kuman Stoykov <kumanstoykov@gmail.com>
2022-04-06 19:54:17 +0200

    Controllers initial

--- JS-WEB/Intro-to-Node.js/resources/index.js
+++ JS-WEB/Intro-to-Node.js/resources/index.js
@@ -14,7 +14,7 @@ const app = http.createServer((req, res) => {
             res.writeHead(200, {
                 'Content-Type': 'text/html'
             });
-            fs.readFile('./views/home/index.html', (err, result) => {
+            fs.readFile('./views/index.html', (err, result) => {
                 if (err) {
                     err.statusCode = 404;
                     return res.end();
