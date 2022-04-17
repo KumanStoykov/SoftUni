342583d9364542b6dd61d3c4a94b42f6e7c63e78
Kuman Stoykov <kumanstoykov@gmail.com>
2022-04-17 16:51:04 +0200

    Create class Cat

---
+++ JS-WEB/ExpressJS and Templating/src/services/catSevices.js
@@ -0,0 +1,25 @@
+const uniqid = require('uniqid');
+
+class Cat {
+    static #cats = [
+
+    ];
+
+    constructor(name, description, image, breed) {
+        this.id = uniqid();
+        this.name = name,
+        this.description = description,
+        this.image = image,
+        this.breed = breed
+    }
+
+    static get cats() {
+        return Cat.#cats.slice();
+    }
+
+    static add(cat) {
+        Cat.#cats.push(cat);
+    }
+}
+
+module.exports = Cat;
