da1b6405d5d741214e08a5e98832cdf66a36006e
Kuman Stoykov <kumanstoykov@gmail.com>
2022-04-17 14:38:45 +0200

    Add CSS

--- JS-WEB/ExpressJS and Templating/src/index.js
+++ JS-WEB/ExpressJS and Templating/src/index.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const path = require('path');
 
 const config = require('./config/config.json')[process.env.NODE_ENV || 'development'];
 
-const initHandlebars = require('./config/handlebars');
+
+const homeController = require('./controllers/homeController');
+
 
 
 const app = express();
+//Handlebars config 
+require('./config/handlebars')(app);
 
-initHandlebars(app);
+app.use(express.static(path.resolve(__dirname, './content')));
+
+
+app.use(homeController);
 
 
 app.listen(config.PORT, console.log.bind(console, `Application is running on http://localhost:${config.PORT}`));
