db49ceaec4aad7c5a8d36ab4c6fb33a6de34c61d
Kuman Stoykov <kumanstoykov@gmail.com>
2022-04-21 18:31:58 +0200

    Initial hard code data file

---
+++ JS-WEB/ExpressJS and Templating/cats.js
@@ -0,0 +1,23 @@
+[
+    {
+    id: '8g06m9ycl24ioqex',
+    name: 'Lisa',
+    description: 'Lisa is good cat.',
+    image: '/images/Lisa.jpg',
+        breed: 'Ocicat'
+    },
+    {
+        id: '8g06mdowl24is4fd',
+        name: 'Misho',
+        description: "Hi, I'm Misho.",
+        image: '/images/Misho.jpg',
+        breed: 'Angor'
+    },
+    {
+        id: '8g06m808l24itiqy',
+        name: 'Lily',
+        description: 'Lily is a best cat ever.',
+        image: '/images/Lily.jpg',
+        breed: 'American bobcat'
+    }
+];
