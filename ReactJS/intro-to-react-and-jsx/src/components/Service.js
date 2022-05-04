f9c866cade841700f6b9af46bc410ee8f46d809d
Kuman Stoykov <kumanstoykov@gmail.com>
2022-05-04 22:34:08 +0200

    Create Service view

---
+++ ReactJS/intro-to-react-and-jsx/src/components/Service.js
@@ -0,0 +1,74 @@
+const Service = () => (
+    <>
+    <div id="service" className="choose_section">
+          <div className="container">
+            <div className="col-sm-12">
+              <h1 className="choose_text">Our<span className="color"> Service</span></h1>
+              <p className="lorem_text">Lorem ipsum dolor sittem ametamngcing elit, per sed do eiusmoad 
+                teimpor sittem elit inuning ut sed.</p>
+            </div>
+          </div>
+        </div>
+        <div className="choose_section_2">
+          <div className="container">
+              <div className="row">
+                <div className="col-sm-4">
+                  <div className="about_inner">
+                    <div className="icon"><a href="#"><img src="images/icon-1.png" /></a></div>
+                    <h2 className="totaly_text">Shared Hosting</h2>
+                    <p className="making">Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci.</p>
+                  </div>
+                </div>
+                <div className="col-sm-4">
+                  <div className="dedicated">
+                    <div className="icon"><a href="#"><img src="images/icon-2.png" /></a></div>
+                    <h2 className="hosting_text">Dedicated Hosting</h2>
+                    <p className="justo_text">Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci.</p>
+                  </div>
+                </div>
+                <div className="col-sm-4">
+                  <div className="about_inner">
+                    <div className="icon"><a href="#"><img src="images/icon-3.png" /></a></div>
+                    <h2 className="totaly_text">Domain Registration</h2>
+                    <p className="making">Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci.</p>
+                  </div>
+                </div>
+              </div>    		
+          </div>
+        </div>
+    
+        <div className="choose_section_2">
+          <div className="container">
+              <div className="row">
+                <div className="col-sm-4">
+                  <div className="about_inner">
+                    <div className="icon"><a href="#"><img src="images/icon-4.png" /></a></div>
+                    <h2 className="totaly_text">Shared Hosting</h2>
+                    <p className="making">Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci.</p>
+                  </div>
+                </div>
+                <div className="col-sm-4">
+                  <div className="about_inner">
+                    <div className="icon"><a href="#"><img src="images/icon-5.png" /></a></div>
+                    <h2 className="totaly_text">Dedicated Hosting</h2>
+                    <p className="making">Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci.</p>
+                  </div>
+                </div>
+                <div className="col-sm-4">
+                  <div className="about_inner">
+                    <div className="icon"><a href="#"><img src="images/icon-6.png" /></a></div>
+                    <h2 className="totaly_text">Domain Registration</h2>
+                    <p className="making">Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci.</p>
+                  </div>
+                </div>
+              </div>
+              <div className="bt_main">
+                <button className="read_more"><a href="#">Read More</a></button>
+                </div>   		
+          </div>
+        </div>
+    
+    </>
+);
+
+export default Service;
