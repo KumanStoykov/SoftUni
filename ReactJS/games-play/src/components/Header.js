30a3a81b1c6517905bfe47d10f285eb9aad3d41f
Kuman Stoykov <kumanstoykov@gmail.com>
2022-05-06 13:42:59 +0200

    Add Header component

---
+++ ReactJS/games-play/src/components/Header.js
@@ -0,0 +1,21 @@
+const Header = () => {
+
+    return (
+        <header>
+            <h1><a className="home" href="#">GamesPlay</a></h1>
+            <nav>
+                <a href="#">All games</a>
+                <div id="user">
+                    <a href="#">Create Game</a>
+                    <a href="#">Logout</a>
+                </div>
+                <div id="guest">
+                    <a href="#">Login</a>
+                    <a href="#">Register</a>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
