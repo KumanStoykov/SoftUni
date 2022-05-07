129a1113911efd12efcd1ef6b186793f920e84ad
Kuman Stoykov <kumanstoykov@gmail.com>
2022-05-07 15:05:34 +0200

    Update Header with react router

--- ReactJS/games-play/src/components/Header.js
+++ ReactJS/games-play/src/components/Header.js
@@ -1,29 +1,19 @@
-const Header = ({
-    navigationChangeHandler,
-}) => {
+import { Link } from 'react-router-dom';
 
-    const onHeaderClick = (e) => {
-        e.preventDefault();
-
-        if(e.target.tagName == 'A') {            
-            let url = new URL(e.target.href);
-            navigationChangeHandler(url.pathname);
-
-        }
-    };
+const Header = () => {   
 
     return (
-        <header onClick={onHeaderClick}>
-            <h1><a className="home" href="/home">GamesPlay</a></h1>
+        <header >
+            <h1><Link className="home" to="/">GamesPlay</Link></h1>
             <nav>
-                <a href="/games">All games</a>
+                <Link to="/games">All games</Link>
                 <div id="user">
-                    <a href="/create-game">Create Game</a>
-                    <a href="/logout">Logout</a>
+                    <Link to="/create-game">Create Game</Link>
+                    <Link to="/logout">Logout</Link>
                 </div>
                 <div id="guest">
-                    <a href="/login">Login</a>
-                    <a href="/register">Register</a>
+                    <Link to="/login">Login</Link>
+                    <Link to="/register">Register</Link>
                 </div>
             </nav>
         </header>
