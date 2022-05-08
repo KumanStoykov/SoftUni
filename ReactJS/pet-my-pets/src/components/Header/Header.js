b82f66060827370787cd69f0fc6a8c2c4c2b10ac
Kuman Stoykov <kumanstoykov@gmail.com>
2022-05-08 16:31:02 +0200

    Make active links to navigate

--- ReactJS/pet-my-pets/src/components/Header/Header.js
+++ ReactJS/pet-my-pets/src/components/Header/Header.js
@@ -1,19 +1,47 @@
+import { Link, NavLink } from 'react-router-dom';
+
 const Header = () => {
+
+    const backgroundIsActive = {
+        background: 'rgb(248, 215, 107)'
+    }
 
     return (
         <header id="site-header">
             <nav className="navbar">
                 <section className="navbar-dashboard">
-                    <a href="#">Dashboard</a>
+                    <Link to="/">Dashboard</Link>
                     <div id="guest">
-                        <a className="button" href="#">Login</a>
-                        <a className="button" href="#">Register</a>
+                        
+                        <NavLink className="button" to="/login"
+                         style={({ isActive }) => (isActive ? backgroundIsActive : null)}
+                        >
+                        Login
+                        </NavLink>
+
+                        <NavLink className="button" to="/register"
+                        style={({ isActive }) => (isActive ? backgroundIsActive : null)}
+                        >
+                        Register
+                        </NavLink>
+
                     </div>
                     <div id="user">
                         <span>Welcome, email</span>
-                        <a className="button" href="#">My Pets</a>
-                        <a className="button" href="#">Add Pet</a>
-                        <a className="button" href="#">Logout</a>
+
+                        <NavLink className="button" to="/my-pets"
+                        style={({ isActive }) => (isActive ? backgroundIsActive : null)}
+                        >
+                        My Pets
+                        </NavLink>
+
+                        <NavLink className="button" to="/create"
+                        style={({ isActive }) => (isActive ? backgroundIsActive : null)}
+                        >
+                        Add Pet
+                        </NavLink>
+
+                        <Link className="button" to="/logout">Logout</Link>
                     </div>
                 </section>
             </nav>
