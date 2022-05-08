13340bdce552812cc32d4bdb03d5538697cd1016
Kuman Stoykov <kumanstoykov@gmail.com>
2022-05-08 16:02:27 +0200

    Add Login component

---
+++ ReactJS/pet-my-pets/src/components/Login/Login.js
@@ -0,0 +1,27 @@
+const Login = () => {
+
+    return (
+        <section id="login-page" className="login">
+            <form id="login-form" action="" method="">
+                <fieldset>
+                    <legend>Login Form</legend>
+                    <p className="field">
+                        <label htmlFor="email">Email</label>
+                        <span className="input">
+                            <input type="text" name="email" id="email" placeholder="Email" />
+                        </span>
+                    </p>
+                    <p className="field">
+                        <label htmlFor="password">Password</label>
+                        <span className="input">
+                            <input type="password" name="password" id="password" placeholder="Password" />
+                        </span>
+                    </p>
+                    <input className="button submit" type="submit" value="Login" />
+                </fieldset>
+            </form>
+        </section>
+    );
+}
+
+export default Login;
