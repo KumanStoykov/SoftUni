cbacd44d1732ebadf5dcbbe6ba8a84c3cd8eec19
Kuman Stoykov <kumanstoykov@gmail.com>
2022-05-10 20:04:10 +0200

    Add create pet

--- ReactJS/pet-my-pets/src/services/petService.js
+++ ReactJS/pet-my-pets/src/services/petService.js
@@ -1,8 +1,9 @@
 
-const baseUrl = 'http://localhost:3030/data';
+const baseUrlData = 'http://localhost:3030/data';
+const baseUrlJsonStore = 'http://localhost:3030/jsonstore';
 
 export const getAll = async () => {
-   let res = await fetch(`${baseUrl}/pets`);
+   let res = await fetch(`${baseUrlData}/pets`);
 
    let pets = await res.json();
 
@@ -10,8 +11,25 @@ export const getAll = async () => {
 };
 
 export const getOne = async (id) => {
-   let res = await fetch(`${baseUrl}/pets/${id}`);
+   let res = await fetch(`${baseUrlData}/pets/${id}`);
 
    let pet = await res.json();
    return pet;
-}
+};
+
+export const create = async (pet) =>{
+   let res = await fetch(`${baseUrlJsonStore}/pets`, {
+      method: 'POST',
+      headers: {
+         'content-type': 'application/json'
+      },
+      body: JSON.stringify({
+         pet
+      })
+
+   });
+   let result = await res.json();
+
+   return result
+   
+};
