2451c300546ccd56477fabf93dcacc473befbb1d
Kuman Stoykov <kumanstoykov@gmail.com>
2022-05-15 18:05:00 +0200

    Add Header

--- ReactJS/logistics-services/src/App.js
+++ ReactJS/logistics-services/src/App.js
@@ -1,10 +1,13 @@
+import Header from './components/Header/Header';
+
+import { Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="site-wrapper">
-     
+     <Header />
      
     </div>
   );
