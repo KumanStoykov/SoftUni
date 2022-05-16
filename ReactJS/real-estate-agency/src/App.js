0839bf0100262e064ca6fd8f56d26328f14713b7
Kuman Stoykov <kumanstoykov@gmail.com>
2022-05-16 13:30:56 +0200

    Create ApartmentsForRecent

--- ReactJS/real-estate-agency/src/App.js
+++ ReactJS/real-estate-agency/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
-import ApartmentsForRecent from './components/ApartmensForRecent/ApartmentsForRecent';
+import ApartmentsForRecent from './components/ApartmentsForRecent/ApartmentsForRecent';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import NotFound from './components/NotFound/NotFound';
