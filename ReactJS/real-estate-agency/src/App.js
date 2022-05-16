6dc824c5155dc31e8059e5a865db5f45b8aa2a27
Kuman Stoykov <kumanstoykov@gmail.com>
2022-05-16 13:35:33 +0200

    Add Create

--- ReactJS/real-estate-agency/src/App.js
+++ ReactJS/real-estate-agency/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import ApartmentsForRecent from './components/ApartmentsForRecent/ApartmentsForRecent';
+import Create from './components/Create/Create';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import NotFound from './components/NotFound/NotFound';
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/apartment-for-recent" element={<ApartmentsForRecent />}/>
+          <Route path="/create" element={<Create />}/>
           <Route path="/login" element={<Login />}/>
           <Route path="/register" element={<Register />}/>
           <Route path="*" element={<NotFound />} />
