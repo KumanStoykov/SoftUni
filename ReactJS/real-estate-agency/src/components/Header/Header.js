a7b0865615aa9880b6cb7895bde5373c105e6260
Kuman Stoykov <kumanstoykov@gmail.com>
2022-05-16 13:00:49 +0200

    Add Header

---
+++ ReactJS/real-estate-agency/src/components/Header/Header.js
@@ -0,0 +1,24 @@
+
+
+const Header = () => {
+
+
+    return (
+        <header>
+            <nav>
+                <a href="/">Home</a>
+                <ul>
+                    <li><a href="/apartment-for-recent">Housing for rent</a></li>
+                    <li><a href="/login">Login</a></li>
+                    <li><a href="/register">Register</a></li>
+                    <li><a href="/create">Create offer</a></li>
+                    <li><a href="/search">Search</a></li>
+                    <li><a href="/logout">Logout</a></li>
+                </ul>
+            </nav>
+        </header>
+
+    );
+}
+
+export default Header;
