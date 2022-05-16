8c1170980606c48d5ccd7811c8fe6665f8469a30
Kuman Stoykov <kumanstoykov@gmail.com>
2022-05-16 13:08:15 +0200

    Create NotFound

--- ReactJS/real-estate-agency/src/App.js
+++ ReactJS/real-estate-agency/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />}/>
           <Route path="/login" element={<Login />}/>
           <Route path="/register" element={<Register />}/>
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
     </div>
---
+++ ReactJS/real-estate-agency/src/components/NotFound/NotFound.js
@@ -0,0 +1,16 @@
+
+
+const NotFound = () => {
+
+
+
+    return (
+        <div class="not-found-container">
+            <h1>404</h1>
+            <h2>Page Not Found</h2>
+            <p>The Page you are looking for doesn't exist or another error occurred. Go to <a href="/">Home Page.</a></p>
+        </div>
+    );
+};
+
+export default NotFound;
