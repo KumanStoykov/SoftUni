6800d318aee0b41596f09493e0ebd0843a598569
Kuman Stoykov <kumanstoykov@gmail.com>
2022-05-17 20:52:59 +0200

    Create templateUtil

---
+++ JS-WEB/Intro-to-Node.js/mobile/src/utils/templateUtil.js
@@ -0,0 +1,31 @@
+const fs = require('fs/promises');
+
+async function loadTemplate(nameOfTemplate) {
+
+    try {        
+        const template = await fs.readFile(`./src/views/${nameOfTemplate}.html`);
+        const str = template.toString();
+        return  str;
+    } catch(err) {
+        return '';
+    }
+};
+
+async function renderLayout(html, isHome) {
+  
+    let layout = await loadTemplate('layout');
+    let search = await loadTemplate('search');
+
+    if(isHome) {
+        layout = layout.replace('{{search}}', search)
+    } else {
+        layout = layout.replace('{{search}}', '')
+
+    }
+
+    return layout.replace('{{body}}', html);
+};
+module.exports = {
+    loadTemplate,
+    renderLayout
+}
