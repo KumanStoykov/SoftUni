67164b331df86e12bd217b6fc93628f47bf0d4d5
Kuman Stoykov <kumanstoykov@gmail.com>
2022-05-17 21:20:25 +0200

    Update read images

--- JS-WEB/Intro-to-Node.js/mobile/src/controllers/contentController.js
+++ JS-WEB/Intro-to-Node.js/mobile/src/controllers/contentController.js
@@ -1,24 +1,29 @@
 const fs = require('fs/promises');
 
 exports.contentController = async (req, res) => {
-    let filename = req.url;
-    console.log(filename)
-    const file = await fs.readFile(`./src/${filename}`);
+    try{
+        const filename = req.url;
+
+        const file = await fs.readFile(`./src/${filename}`);
+        
+        let type;
+        
+        if(filename.includes('css')) {
+            type = 'text/css';        
+        } else if(filename.includes('jpg') || filename.includes('jpeg')){
+            type = 'image/jpg'
+        } else if(filename.includes('png')) {
+            type = 'image/png';
+        }
     
-    let type;
     
-    if(filename.includes('css')) {
-        type = 'text/css';        
-    } else if(filename.includes('jpg') || filename.includes('jpeg')){
-        type = 'image/jpg'
-    } else if(filename.includes('png')) {
-        type = 'image/png';
+        res.writeHead(200, {
+            'Content-type': type
+        });
+        res.write(file.toString())
+        res.end();
+
+    } catch(err) {
+        console.log(err)
     }
-
-
-    res.writeHead(200, {
-        'Content-type': type
-    });
-    res.write(file.toString())
-    res.end();
 }
