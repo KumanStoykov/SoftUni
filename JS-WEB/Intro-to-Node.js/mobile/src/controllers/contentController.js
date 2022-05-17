f6a670dd503dfee2ab7dfc48b437c9671f0d7c31
Kuman Stoykov <kumanstoykov@gmail.com>
2022-05-17 21:11:41 +0200

    Update to read images

--- JS-WEB/Intro-to-Node.js/mobile/src/controllers/contentController.js
+++ JS-WEB/Intro-to-Node.js/mobile/src/controllers/contentController.js
@@ -1,10 +1,23 @@
 const fs = require('fs/promises');
 
 exports.contentController = async (req, res) => {
-    let file = await fs.readFile('./src/content/styles/site.css');
+    let filename = req.url;
+    console.log(filename)
+    const file = await fs.readFile(`./src/${filename}`);
+    
+    let type;
+    
+    if(filename.includes('css')) {
+        type = 'text/css';        
+    } else if(filename.includes('jpg') || filename.includes('jpeg')){
+        type = 'image/jpg'
+    } else if(filename.includes('png')) {
+        type = 'image/png';
+    }
+
 
     res.writeHead(200, {
-        'Content-type': 'text/css'
+        'Content-type': type
     });
     res.write(file.toString())
     res.end();
