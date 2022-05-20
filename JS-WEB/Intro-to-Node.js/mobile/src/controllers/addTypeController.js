16c7b54da858890cc02fe002a8c053512e19b072
Kuman Stoykov <kumanstoykov@gmail.com>
2022-05-20 20:53:47 +0200

    Add type request

--- JS-WEB/Intro-to-Node.js/mobile/src/controllers/addTypeController.js
+++ JS-WEB/Intro-to-Node.js/mobile/src/controllers/addTypeController.js
@@ -1,14 +1,38 @@
 const { loadTemplate, renderLayout } = require('../utils/templateUtil');
 
+const formidable = require('formidable');
+const dataService = require('../services/dataServices');
+
 
 exports.addTypeController = async (req, res) => {
 
-    let addType = await loadTemplate('addType');
+    if (req.method == 'POST') {
+        const form = formidable({ multiples: true });
 
-    res.writeHead(200, {
-        'Content-type': 'text/html'
-    });
-    res.write(await renderLayout(addType));
+        form.parse(req, (err, fields, files) => {
+            if (err) {
+                res.writeHead(err.httpCode || 400, { 'Content-type': 'text/plain' });
+                res.end(String(err));
+                return;
+            }
+            dataService.createType({type: fields.type });
+            res.writeHead(302, {
+                'Location': 'add-type'
+            });
+            res.end();
 
-    res.end();
+        });
+
+    } else {
+
+        let addType = await loadTemplate('addType');
+
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        });
+        res.write(await renderLayout(addType));
+
+        res.end();
+    }
+
 };
