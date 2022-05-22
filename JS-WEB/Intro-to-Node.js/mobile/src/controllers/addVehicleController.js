1561d4dea2e769aa165f7e825b9bfd6d6f4d2c58
Kuman Stoykov <kumanstoykov@gmail.com>
2022-05-22 13:50:58 +0200

    Update

--- JS-WEB/Intro-to-Node.js/mobile/src/controllers/addVehicleController.js
+++ JS-WEB/Intro-to-Node.js/mobile/src/controllers/addVehicleController.js
@@ -7,45 +7,53 @@ const dataService = require('../services/dataServices');
 
 
 exports.addVehicleController = async (req, res) => {
-
-    if (req.method == 'POST') {
-        const form = formidable({ multiples: true });
-
-        form.parse(req, async (err, fields, files) => {
-            if (err) {
-                res.writeHead(err.httpCode || 400, { 'Content-type': 'text/plain' });
-                res.end(String(err));
-                return;
-            }
-            const filepath = files.upload.filepath;
-
-            const originalFilename = files.upload.originalFilename;
-            const targetPath = './src/content/images/' + originalFilename;
-
-            await fs.rename(filepath, targetPath);
-
-            fields.imageUrl = targetPath.slice(5);
-
-            dataService.createVehicle({ ...fields });
-
-            res.writeHead(302, {
-                'Location': '/'
+    try {
+        if (req.method == 'POST') {
+            const form = formidable({ multiples: true });
+    
+            form.parse(req, async (err, fields, files) => {
+                if (err) {
+                    res.writeHead(err.httpCode || 400, { 'Content-type': 'text/plain' });
+                    res.end(String(err));
+                    return;
+                }
+                const filepath = files.upload.filepath;
+    
+                const originalFilename = files.upload.originalFilename;
+                const targetPath = './src/content/images/' + originalFilename;
+    
+                await fs.rename(filepath, targetPath);
+    
+                fields.imageUrl = targetPath.slice(5);
+    
+                dataService.createVehicle({ ...fields });
+    
+                res.writeHead(302, {
+                    'Location': '/'
+                });
+                res.end();
             });
+    
+        } else {
+            let addVehicle = await loadTemplate('addVehicle');
+            let typeData = await dataServices.getTypes();
+            let addTypes = typeData.map(x => `<option value="${x.type}">${x.type}</option>`).join('');
+    
+            addVehicle = addVehicle.replace('{{type}}', addTypes);
+    
+    
+            res.writeHead(200, {
+                'Content-type': 'text/html'
+            });
+            res.write(await renderLayout(addVehicle)); 
+    
             res.end();
+        }
+
+    } catch(err) {
+        res.writeHead(302, {
+            location: '/error'
         });
-
-    } else {
-        let addVehicle = await loadTemplate('addVehicle');
-        let typeData = await dataServices.getTypes();
-
-        addVehicle = addVehicle.replace('{{type}}', typeData.map(x => `<option value="${x.type}">${x.type}</option>`).join(''));
-
-
-        res.writeHead(200, {
-            'Content-type': 'text/html'
-        });
-        res.write(await renderLayout(addVehicle));
-
         res.end();
     }
 
