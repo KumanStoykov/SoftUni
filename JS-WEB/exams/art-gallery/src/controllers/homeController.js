7aa020043a9c526cf9f73f027d9de8d3587c0e92
Kuman Stoykov <kumanstoykov@gmail.com>
2022-05-25 18:01:49 +0200

    Dynamic add cards

--- JS-WEB/exams/art-gallery/src/controllers/homeController.js
+++ JS-WEB/exams/art-gallery/src/controllers/homeController.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
 
-const home = (req, res) => {
-    res.render('home');
+const publicationService = require('../services/publicationService');
+
+
+const home = async (req, res) => {
+
+   let publications = await publicationService.getAll();
+
+    res.render('home', { publications });
 };
 
 router.get('/', home);
