5ec7c52d5ca49e649df1118df180d424ec758a3f
Kuman Stoykov <kumanstoykov@gmail.com>
2022-05-25 16:40:55 +0200

    Add routes

--- JS-WEB/exams/art-gallery/src/router.js
+++ JS-WEB/exams/art-gallery/src/router.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
 const homeController = require('./controllers/homeController');
 const galleryController = require('./controllers/galleryController');
+const publicationsController = require('./controllers/publicationsController');
+const authController = require('./controllers/authController');
 
 
 router.use(homeController);
 router.use('/gallery', galleryController);
+router.use('/publication', publicationsController);
+router.use('/auth', authController);
 router.use('*', (req, res) => {
     res.status(404).render('404');
 });
