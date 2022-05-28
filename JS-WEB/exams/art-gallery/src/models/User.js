a6c7127edf3abb23ea8b55d230a18e6078d1432d
Kuman Stoykov <kumanstoykov@gmail.com>
2022-05-28 15:58:52 +0200

    Create User model

---
+++ JS-WEB/exams/art-gallery/src/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    myPublications: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: 'Publication'
+        }
+    ]
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
