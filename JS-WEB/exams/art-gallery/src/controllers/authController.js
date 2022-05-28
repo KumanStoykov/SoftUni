a042c6860ca5fd04940a955c0afb443e93f9c940
Kuman Stoykov <kumanstoykov@gmail.com>
2022-05-28 15:56:25 +0200

    Create login post

--- JS-WEB/exams/art-gallery/src/controllers/authController.js
+++ JS-WEB/exams/art-gallery/src/controllers/authController.js
@@ -3,7 +3,19 @@ const router = require('express').Router();
 
 const login = (req, res) => {
     res.render('auth/login');
-};
+});
+
+router.post('/login', async (req, res) => {
+    let { username, password } = req.body;
+
+
+    let user = await authService.login(username, password);
+
+    if(!user) {
+        return res.redirect('/404');
+    }
+
+    let token = await authService.crateToken(user);
 
 const register = (req, res) => {
     res.render('auth/register');
