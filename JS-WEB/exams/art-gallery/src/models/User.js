2e95a6b41ed3a4b291380c03fbdc659e7d30d70d
Kuman Stoykov <kumanstoykov@gmail.com>
2022-05-29 13:00:18 +0200

    Added myShared

--- JS-WEB/exams/art-gallery/src/models/User.js
+++ JS-WEB/exams/art-gallery/src/models/User.js
@@ -19,6 +19,12 @@ const userSchema = new mongoose.Schema({
             type: mongoose.Types.ObjectId,
             ref: 'Publication'
         }
+    ],
+    myShared: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: 'Publication'
+        }
     ]
 });
 
