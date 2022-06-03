80f45e6b69a68df1707e6904f022ca1a214d86d7
Kuman Stoykov <kumanstoykov@gmail.com>
2022-06-03 11:12:26 +0200

    Set handlebars

---
+++ JS-WEB/exams/real-estate-agency/src/config/handlebars.js
@@ -0,0 +1,13 @@
+const handlebars = require('express-handlebars');
+const path = require('path');
+
+module.exports = (app) => {
+    app.set('views', path.resolve('./src/views'));
+
+    const hbs = handlebars.create({
+        extname: 'hbs'
+    });
+
+    app.engine('hbs', hbs.engine);
+    app.set('view engine', 'hbs');
+}
