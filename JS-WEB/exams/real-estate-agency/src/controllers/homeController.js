7d0dc1f0b4a9097af975e24e19eeba880b01fac0
Kuman Stoykov <kumanstoykov@gmail.com>
2022-06-04 16:38:49 +0200

    Added aprt-for-recent

--- JS-WEB/exams/real-estate-agency/src/controllers/homeController.js
+++ JS-WEB/exams/real-estate-agency/src/controllers/homeController.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', { title: 'Home'});
+});
+
+router.get('/aprt-for-recent', (req, res) => {
+    res.render('aprt-for-recent', { title: 'Apartments for recents'});
 });
 
 module.exports = router;
