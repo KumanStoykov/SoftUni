f959547d18b29e75094600e1a98a0fe41dd82eb6
Kuman Stoykov <kumanstoykov@gmail.com>
2022-06-05 14:02:05 +0200

    Create Housing model

---
+++ JS-WEB/exams/real-estate-agency/src/models/Housing.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const housingSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        //TODO: Required 'Apartment', 'Villa', 'House'
+        required: true
+    },
+    year: {
+        type: Number,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    availablePieces: {
+        type: Number,
+        required: true
+    },
+    Owner: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    },
+    rentedHome: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: 'User',
+        }
+    ]
+});
+
+const Housing = mongoose.model('Housing', housingSchema);
+
+module.exports = Housing;
