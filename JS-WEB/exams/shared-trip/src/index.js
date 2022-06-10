f5609fe429340a76a726fe87f4054719eb97ff2e
Kuman Stoykov <kumanstoykov@gmail.com>
2022-06-10 18:10:54 +0200

    Added expressConfig file

--- JS-WEB/exams/shared-trip/src/index.js
+++ JS-WEB/exams/shared-trip/src/index.js
@@ -2,12 +2,15 @@ const express = require('express');
 
 const initDatabase = require('./config/initMongodb');
 const configJson = require('./config/config.json')[process.env.NODE_EVN];
+const expressConfig = require('./config/expressConfig');
 
 
 const app = express();
 
+expressConfig(app);
+
 initDatabase(configJson.CONNECTION_STRING)
     .then(() => {
-        app.listen(configJson.PORT, console.log.bind(console, `App is listen in port:${configJson.PORT}`))
+        app.listen(configJson.PORT, console.log.bind(console, `App is listen in port:${configJson.PORT}...`))
     })
-    .catch(error => console.log('Initial database is field!', error));
+    .catch(error => console.log('Initial database field!', error));
