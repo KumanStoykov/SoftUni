3088dd6041d390d7d6b6b792ca5f077299655380
Kuman Stoykov <kumanstoykov@gmail.com>
2022-06-10 18:09:14 +0200

    Config static config

--- JS-WEB/exams/shared-trip/src/config/expressConfig.js
+++ JS-WEB/exams/shared-trip/src/config/expressConfig.js
@@ -1,16 +1,19 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 const initHandlebars = require('./initHandlebars');
-const cookieParser = require('cookie-parser');
 const router = require('./router');
 
 module.exports = (app) => {
+    
+    initHandlebars(app);
+
+    app.use(express.static('./src/static'));
 
     app.use(express.urlencoded({ extended: true }));
 
     app.use(cookieParser());
 
-    app.use(initHandlebars(app));
 
     app.use(router);
 }
