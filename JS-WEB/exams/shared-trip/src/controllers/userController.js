974c6f50709f33e7489490cf03a5355d5f67ef9d
Kuman Stoykov <kumanstoykov@gmail.com>
2022-06-11 17:33:25 +0200

    Added error handling

--- JS-WEB/exams/shared-trip/src/controllers/userController.js
+++ JS-WEB/exams/shared-trip/src/controllers/userController.js
@@ -1,16 +1,32 @@
 const router = require('express').Router();
+const validator = require('validator').default;
 
 const userServices = require('../services/userServices');
 const { COOKIE_TOKEN_NAME } = require('../constants');
+const { isGuest, isAuth } = require('../middlewares/authGuardsMiddleware');
 
-router.get('/login', (req, res) => {
+
+router.get('/login', isGuest, (req, res) => {
    res.render('user/login', { title: 'Login page' });
 });
 
-router.post('/login', async (req, res) => {
+router.post('/login', isGuest, async (req, res) => {
 
    try {
       const { email, password } = req.body;
+      const errors = [];
+
+      if(!validator.isEmail(email)){
+         errors.push('The email should be in the following format "username@domain.bg');
+      }
+
+      if(!validator.isLength(password, { min: 4 })) {
+         errors.push('The password should be at least 4 characters long');
+      }
+
+      if(errors.length > 0) {
+         throw new Error(errors.join(', '));
+      }
 
       const token = await userServices.login(email, password);
 
@@ -25,13 +41,11 @@ router.post('/login', async (req, res) => {
       res.redirect('/');
 
    } catch (error) {
-
-      res.render('404', { title: 'Error' });
-
+      res.render('user/login', { title: 'Login page', errors: error.message.split(', '), data: req.body });
    }
 });
 
-router.get('/register', (req, res) => {
+router.get('/register', isGuest, (req, res) => {
    res.render('user/register', { title: 'Register page' });
 });
 
@@ -39,6 +53,24 @@ router.post('/register', async (req, res) => {
 
    try {
       const { email, password, repeatPassword, gender } = req.body;
+
+      const errors = [];
+
+      if(!validator.isEmail(email)){
+         errors.push('The email should be in the following format "username@domain.bg');
+      }
+
+      if(!validator.isLength(password, { min: 4 })) {
+         errors.push('The password should be at least 4 characters long');
+      }
+
+      if(!validator.equals(password, repeatPassword)) {
+         errors.push('The repeat password should be equal to the password');
+      }
+
+      if(errors.length > 0) {
+         throw new Error(errors.join(', '));
+      }
 
       await userServices.register(email, password, gender);
 
@@ -56,18 +88,30 @@ router.post('/register', async (req, res) => {
       res.redirect('/');
 
    } catch (error) {
-      res.render('404', { title: 'Error' });
+      res.render('user/register', { title: 'Register page', errors: error.message.split(', '), data: req.body });
+
    }
 });
 
-router.get('/logout', (req, res) => {
+router.get('/logout', isAuth, (req, res) => {
    
    res.clearCookie(COOKIE_TOKEN_NAME);
    res.redirect('/');
 });
 
-router.get('/profile', (req, res) => {
-   res.render('user/profile', { title: 'Profile page' });
+router.get('/profile', isAuth, async (req, res) => {
+
+   try{
+      const user = await userServices.getUser(req.user._id);
+
+      user.tripsCount = user.trips.length;
+      console.log(user.trips.length)
+
+      res.render('user/profile', { title: 'Profile page', ...user})
+
+   } catch(error) {
+      res.render('404', { title: 'Error'});
+   }
 });
 
 
