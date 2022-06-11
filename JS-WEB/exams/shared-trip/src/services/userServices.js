18ca86e4788286770805dfc784e2c40c495f2d56
Kuman Stoykov <kumanstoykov@gmail.com>
2022-06-11 11:14:19 +0200

    Create user service

---
+++ JS-WEB/exams/shared-trip/src/services/userServices.js
@@ -0,0 +1,39 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
+
+const { SALT_ROUND } = require('../constants');
+const { createToken } = require('../utility/jwtUtility');
+
+exports.register = async (email, password, gender) => {
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUND);
+
+    const userReg = {
+        email,
+        password: hashedPassword,
+        gender,
+        trips: []
+    };
+
+    return User.create(userReg);
+}
+
+exports.login = async (email, password) => {
+
+    const user = await User.findOne({ email });
+
+    if (!user){
+        throw new Error('Cannot find email or password!'); 
+    }
+
+    const comparePassword = await bcrypt.compare(password, user.password);
+
+    if(!comparePassword) {
+        throw new Error("Email or password don\'t match!")
+    }
+
+    
+    const token = await createToken(user);
+    
+    return token;
+}
