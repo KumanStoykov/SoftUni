39e20bc5413a15993a36c1fb462951d751a876c1
Kuman Stoykov <kumanstoykov@gmail.com>
2022-06-11 11:31:14 +0200

    Create Trip model

---
+++ JS-WEB/exams/shared-trip/src/models/Trip.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+
+const tripSchema = new mongoose.Schema({
+    startPoint: {
+        type: String,
+        required: true
+    },
+    endPoint: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        required: true
+    },
+    time: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    carBrand: {
+        type: String,
+        required: true
+    },
+    seats: {
+        type: Number,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    creator: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    },
+    buddies: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+
+});
+
+const Trip = mongoose.model('Trip', tripSchema);
+
+module.exports = Trip;
