e19f00f102c8f0a9d468dbe44478ed5832dd4ba0
Kuman Stoykov <kumanstoykov@gmail.com>
2022-06-16 22:05:37 +0200

    Create home controller

--- JS-WEB/exams/wild-life/src/controllers/homeController.js
+++ JS-WEB/exams/wild-life/src/controllers/homeController.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
 
+const postService = require('../services/postServices');
+
 router.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', { title: 'Home page'});
+    
+});
+
+router.get('/all-posts', async (req, res) => {
+
+    try{
+        const posts = await postService.getAll();
+
+        res.render('all-posts', { title: 'Home page', posts});
+    } catch(error) {
+        res.redirect('404', { title: 'Error'});
+    }
     
 });
 
