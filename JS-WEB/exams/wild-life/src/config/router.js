fda8f23bd5c5f014754d905ddbfd0c10c6fee908
Kuman Stoykov <kumanstoykov@gmail.com>
2022-06-16 22:06:21 +0200

    Added other routes

--- JS-WEB/exams/wild-life/src/config/router.js
+++ JS-WEB/exams/wild-life/src/config/router.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
 
 const homeController = require('../controllers/homeController');
+const postController = require('../controllers/postController');
+const userController = require('../controllers/userController');
 
-router.use(homeController)
+router.use(homeController);
+router.use('/post', postController);
+router.use('/user', userController);
+
+router.use('*', (req, res) => {
+   res.render('404', { title: 'Not found'}); 
+});
 
 module.exports = router;
