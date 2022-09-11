af2c438e0c0c8935807d849df672ae8b719231f3
Kuman Stoykov <kumanstoykov@gmail.com>
2022-09-11 22:12:21 +0200

    Create theme interface

---
+++ Angular/Components-Workshop/client/src/app/interfaces/theme.ts
@@ -0,0 +1,9 @@
+export interface ITheme {
+    _id: string;
+    subscribes: string[];
+    posts: string[];
+    themeName: string;
+    userId: string;
+    created_at: Date;
+    updateAt: Date;
+}
