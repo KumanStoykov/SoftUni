338cc3d56d1fcc6757da157f940b2e0366d7cf6b
Kuman Stoykov <kumanstoykov@gmail.com>
2022-09-11 22:11:14 +0200

    Create forum service

---
+++ Angular/Components-Workshop/client/src/app/forum.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ITheme } from './interfaces/theme';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class ForumService {
+
+    constructor(private http: HttpClient) { }
+
+    loadThemes() {
+        return this.http.get<ITheme[]>(`http://localhost:3000/api/themes`);
+    }
+}
