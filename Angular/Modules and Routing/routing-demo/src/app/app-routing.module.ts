9b5edd1168851e02ecc4405c076fa7e415ee8ec2
Kuman Stoykov <kumanstoykov@gmail.com>
2022-09-15 14:58:56 +0200

    Config routing

--- Angular/Modules and Routing/routing-demo/src/app/app-routing.module.ts
+++ Angular/Modules and Routing/routing-demo/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { UserListComponent } from './user/user-list/user-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/user-list'
+  },
+  {
+    path: 'user-list',
+    component: UserListComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Angular/Modules and Routing/routing-demo/src/app/app.module.ts
+++ Angular/Modules and Routing/routing-demo/src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { UserService } from './user.service';
+import { UserModule } from './user/user.module';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    SharedModule,
+    UserModule
   ],
-  providers: [],
+  providers: [
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
