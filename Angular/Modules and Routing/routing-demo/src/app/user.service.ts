2073d22961783145a3717f20f06b41132f0e0d38
Kuman Stoykov <kumanstoykov@gmail.com>
2022-09-15 14:56:11 +0200

    Create user services

---
+++ Angular/Modules and Routing/routing-demo/src/app/user.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IUser } from './shared/interfaces/user';
+
+@Injectable()
+
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  loadUsers(search: string = '') {
+    const query = search ? `?email_like=${search}` : '';
+    return this.http.get<IUser[]>(`https://jsonplaceholder.typicode.com/users${query}`);
+  }
+}
