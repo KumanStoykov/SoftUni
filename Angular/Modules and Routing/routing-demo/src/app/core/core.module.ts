14c888ff348ad28a922adeaef3181b8462f4dd6d
Kuman Stoykov <kumanstoykov@gmail.com>
2022-09-15 14:58:03 +0200

    Create core module

---
+++ Angular/Modules and Routing/routing-demo/src/app/core/core.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TimeComponent } from './time/time.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    TimeComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent
+  ]
+})
+export class CoreModule { }
