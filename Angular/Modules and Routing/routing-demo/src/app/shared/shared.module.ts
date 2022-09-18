7436fc887ce8be017794e250e07299aa63167301
Kuman Stoykov <kumanstoykov@gmail.com>
2022-09-18 15:56:45 +0200

    Create guard router

--- Angular/Modules and Routing/routing-demo/src/app/shared/shared.module.ts
+++ Angular/Modules and Routing/routing-demo/src/app/shared/shared.module.ts
@@ -1,12 +1,26 @@
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoaderComponent } from './loader/loader.component';
+import { ParamsActivate } from './guards/params.activate';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoaderComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    LoaderComponent
   ]
 })
-export class SharedModule { }
+export class SharedModule {
+    static forRoot(): ModuleWithProviders<SharedModule> {
+        return {
+            ngModule: SharedModule,
+            providers: [ParamsActivate]
+        }
+    }
+}
