d0e61460a4785f2c0516e65ad110910a6436f37e
Kuman Stoykov <kumanstoykov@gmail.com>
2022-09-18 15:59:28 +0200

    Create test two component

---
+++ Angular/Modules and Routing/routing-demo/src/app/test/two/two.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TwoComponent } from './two.component';
+
+describe('TwoComponent', () => {
+  let component: TwoComponent;
+  let fixture: ComponentFixture<TwoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TwoComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TwoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
