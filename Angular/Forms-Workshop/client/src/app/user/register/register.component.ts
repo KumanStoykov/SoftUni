ae254960c8a3732c157a15e5892a44c3da77122a
Kuman Stoykov <kumanstoykov@gmail.com>
2022-10-04 21:42:50 +0200

    Added notifiacations

--- Angular/Forms-Workshop/client/src/app/user/register/register.component.ts
+++ Angular/Forms-Workshop/client/src/app/user/register/register.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { faUser, faPhone, faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
+import { Subject } from 'rxjs';
+import { sameValueAsFactory, emailValidator } from 'src/app/shared/validators';
 import { UserService } from '../user.service';
 
 
@@ -8,7 +12,7 @@ import { UserService } from '../user.service';
     templateUrl: './register.component.html',
     styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnDestroy {
 
     icons = {
         faUser,
@@ -17,12 +21,49 @@ export class RegisterComponent {
         faLock
     }
 
-    constructor(private userService: UserService) { }
+    killSubscription = new Subject();
 
+    form: FormGroup;
 
-    register(): void {
+    constructor(
+        private userService: UserService,
+        private fb: FormBuilder,
+        private router: Router
+    ) {
+        this.form = this.fb.group({
+            username: ['', [Validators.required, Validators.minLength(6)]],
+            email: ['', [Validators.required, emailValidator]],
+            tel: [''],
+            //Second variant with group, then need to select in html group element
+            // group: this.fb.group({
+            //     password: ['', [Validators.required, Validators.minLength(4)]],
+            //     rePassword: ['', [Validators.required, sameValueAsFactory(
+            //         () => this.form?.get('password'), this.killSubscription
+            //     )]]
 
+            // }),
+            password: ['', [Validators.required, Validators.minLength(4)]],
+            rePassword: ['', [Validators.required, sameValueAsFactory(
+                () => this.form?.get('password'), this.killSubscription
+            )]]
+        });
     }
 
 
+    register(): void {
+        if (!this.form.valid) { return; }
+        this.userService.register(this.form.value).subscribe({
+            next: () => {
+                this.router.navigate(['/']);
+            },
+            error: (err) => {
+                console.log(err);
+            }
+        })
+    }
+
+    ngOnDestroy(): void {
+        this.killSubscription.next('');
+        this.killSubscription.complete();
+    }
 }
