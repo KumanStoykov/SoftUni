b0e8cd75707f9cb672cbc84b3c8c46c5257ed985
Kuman Stoykov <kumanstoykov@gmail.com>
2022-12-24 17:02:26 +0100

    Added exercise 08-Beer-Kegs

---
+++ Java Fundamentel/src/dataTypes/exercise/BeerKegs.java
@@ -0,0 +1,27 @@
+package dataTypes.exercise;
+
+import java.util.Scanner;
+
+public class BeerKegs {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        byte groups = Byte.parseByte(scanner.nextLine());
+        String biggestKeg = "";
+        double biggestVolume = 0;
+
+        for (int i = 0; i < groups; i++) {
+            String model = scanner.nextLine();
+            float radius = Float.parseFloat(scanner.nextLine());
+            int height = Integer.parseInt(scanner.nextLine());
+
+            double volume = Math.PI * Math.pow(radius, 2) * height;
+
+            if (volume > biggestVolume) {
+                biggestVolume = volume;
+                biggestKeg = model;
+            }
+        }
+        System.out.println(biggestKeg);
+    }
+}
