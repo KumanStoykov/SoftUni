e797f01ace4b386e76e90b92329547ef4215e036
Kuman Stoykov <kumanstoykov@gmail.com>
2022-12-27 20:09:56 +0100

    Added exercise houseParty

---
+++ Java Fundamentel/src/list/exercise/houseParty.java
@@ -0,0 +1,36 @@
+package list.exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class houseParty {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int countRows = Integer.parseInt(scanner.nextLine());
+
+        List<String> gastList = new ArrayList<>();
+
+        for (int i = 0; i < countRows; i++) {
+            String[] people = scanner.nextLine().split(" is ");
+            String name = people[0];
+            String command = people[people.length - 1];
+
+            if (command.equals("not going!")) {
+                if (gastList.contains(name)){
+                    gastList.remove(name);
+                } else {
+                    System.out.printf("%s is not in the list!%n", name);
+                }
+            } else {
+                if (!gastList.contains(name)){
+                    gastList.add(name);
+                } else {
+                    System.out.printf("%s is already in the list!%n", name);
+                }
+            }
+        }
+        gastList.forEach(System.out::println);
+    }
+}
