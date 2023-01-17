7381a262d1ff1087da8985d561edd57c50dc164d
Kuman Stoykov <kumanstoykov@gmail.com>
2023-01-17 12:47:39 +0100

    Update Phonebook.java

--- Java Advanced/src/setsAndMaps/Phonebook.java
+++ Java Advanced/src/setsAndMaps/Phonebook.java
@@ -5,28 +5,29 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Scanner;
 
-public class PhoneBook {
+public class Phonebook {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Map<String, String> phoneBook = new HashMap<>();
+        Map<String, String> phonebook = new HashMap<>();
 
         String untilSearch = scanner.nextLine();
         while (!untilSearch.equals("search")) {
             String name = untilSearch.split("-")[0];
             String number = untilSearch.split("-")[1];
-            phoneBook.putIfAbsent(name, number);
+            phonebook.putIfAbsent(name, number);
 
             untilSearch = scanner.nextLine();
         }
         String untilStop = scanner.nextLine();
         while (!untilStop.equals("stop")) {
+            
             String name = untilStop;
 
-            if (phoneBook.containsKey(name)) {
-                phoneBook.forEach((key, value) -> {
-                    if (Objects.equals(phoneBook.get(name), value)) {
+            if (phonebook.containsKey(name)) {
+                phonebook.forEach((key, value) -> {
+                    if (Objects.equals(phonebook.get(name), value)) {
                         System.out.printf("%s -> %s%n", key, value);
                     }
                 });
