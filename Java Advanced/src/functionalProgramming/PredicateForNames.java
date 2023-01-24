bff61ef8726121181ba2a40eea403614229d285b
Kuman Stoykov <kumanstoykov@gmail.com>
2023-01-24 12:15:18 +0100

    Added exercise PredicateForNames

---
+++ Java Advanced/src/functionalProgramming/PredicateForNames.java
@@ -0,0 +1,19 @@
+package functionalProgramming;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Predicate;
+
+public class PredicateForNames {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int maxLength = Integer.parseInt(scanner.nextLine());
+        Predicate<String> checkLength = word -> word.length() <= maxLength;
+
+        String[] names = scanner.nextLine().split("\\s+");
+
+        Arrays.stream(names).filter(checkLength).forEach(w -> System.out.println(w));
+    }
+}
