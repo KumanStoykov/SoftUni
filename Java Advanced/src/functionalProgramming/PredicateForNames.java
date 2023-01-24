03f830ce84779232c73ecbad4a1ed42d25ac88af
Kuman Stoykov <kumanstoykov@gmail.com>
2023-01-24 12:15:57 +0100

    Update print

--- Java Advanced/src/functionalProgramming/PredicateForNames.java
+++ Java Advanced/src/functionalProgramming/PredicateForNames.java
@@ -14,6 +14,6 @@ public class PredicateForNames {
 
         String[] names = scanner.nextLine().split("\\s+");
 
-        Arrays.stream(names).filter(checkLength).forEach(w -> System.out.println(w));
+        Arrays.stream(names).filter(checkLength).forEach(System.out::println);
     }
 }
